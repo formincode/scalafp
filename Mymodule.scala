25508ca1a89bd40b820c0a657fb6b70f3d96908e
Manish Srivastava <manish.srivastava@manisriv-mbp.enernoc.local>
2015-05-27 13:11:53 -0400

    My first class

---
+++ Mymodule.scala
@@ -0,0 +1,15 @@
+
+object Mymodule {
+
+  def abs(n:Int): Int = 
+    if (n<0) -n
+    else n
+
+  private def formatAbs(x:Int) = {
+     val msg = "The abs of %d is %d"
+     msg.format(x,abs(x))
+  }
+
+  def main(args: Array[String]): Unit = 
+    println(formatAbs(-42))
+}
