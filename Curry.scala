2b2ffd4d03f1ce23deba38895e41d4fa3508a253
Manish Srivastava <manish.srivastava@MANISRIV-MBP.local>
2015-05-30 20:19:10 -0400

    Fixing stuff

--- Curry.scala
+++ Curry.scala
@@ -1,7 +1,3 @@
 /**
  * Created by manish.srivastava on 5/28/15.
  */
-
-def curry[A,B,C](f: (A, B) => C): A => (B => C) = a => (b => f(a,b))
-def uncurry[A,B,C](f: A => B => C): (A, B) => C = (a,b) => f(a)(b)
-def compose[A,B,C](f: B => C, g: A => B): A => C = A => f(g(A))
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # scalafp
+https://github.com/fpinscala/fpinscala/tree/master/answerkey
 https://github.com/fpinscala/fpinscala/wiki
